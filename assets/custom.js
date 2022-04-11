7d34d72a037851c0efe3dad4018865c3ccd5032d
khawarcp <khawaraneescp@gmail.com>
2022-04-11 12:32:49 +0500

    Ajax Load More Button Working

---
+++ assets/custom.js
@@ -0,0 +1,25 @@
+var products_on_page = $('products-on-page');
+var next_url = products_on_page.data('next-url');
+var load_more_btn = $('.load-more__btn');
+var load_more_spinner = $('.load-more__spinner');
+function loadMoreProducts() {
+$.ajax({
+    type : 'GET',
+    url : next_url,
+    dataType : 'html',
+    beforeSend : function(){
+        load_more_btn.hide();
+        load_more_spinner.show();
+    }
+}).done(function(next_page) {
+
+    load_more_spinner.hide();
+    var new_products = $(next_page).find('.products-on-page');
+    var new_url = new_products.data('next-url');
+    if(new_url){
+        load_more_btn.show();
+    }
+    new_url = next_url;
+    products_on_page.append(new_products.html());
+});
+}
