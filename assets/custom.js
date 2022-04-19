c9d8496e1cc6b5542e5f6654601cad22eae98dcb
khawarcp <khawaraneescp@gmail.com>
2022-04-19 10:04:29 +0500

    Carousel Moved

--- assets/custom.js
+++ assets/custom.js
@@ -38,3 +38,46 @@ $('ul.tabs li').click(function(){
   $("#"+tab_id).addClass('current');
 })
 
+
+  $('.testimoinals-carousel').slick({
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    navs:false,
+    centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
+
+      }
+
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        infinite: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }]
+  });
+
+
+
